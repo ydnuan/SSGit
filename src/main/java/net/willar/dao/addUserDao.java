43f03cf154be89a08fb84c30dd85fa2a44da3d2c
ydnuan <724939338@qq.com>
2016-11-14 18:09:50 +0800

    第一次提交

---
+++ src/main/java/net/willar/dao/addUserDao.java
@@ -0,0 +1,19 @@
+/*
+ +---------------------------------------------------------------------+
+ | 维纳网 [ 一维纳世界 一网纳天下 ]
+ +---------------------------------------------------------------------+
+ | Copyright (c) 2014-2015 http://www.willar.net All rights reserved.
+ +---------------------------------------------------------------------+
+ | Author: HB <hubo@willar.net>
+ +---------------------------------------------------------------------+
+*/
+
+package net.willar.dao;
+
+/**
+ * Created by yangxuri_HB on 16/11/14.
+ */
+public interface addUserDao {
+    public void insert(String username, String userPwd);
+
+}
